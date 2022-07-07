420fe9407e44c26e8571f1deeffa549a2147ef83
devthalal <thalal.hassan@neoito.com>
2022-07-07 12:26:39 +0530

    test

--- src/todoInputSaaS.js
+++ src/todoInputSaaS.js
@@ -6,7 +6,7 @@ export const TodoInputSaaS = ({ refetch }) => {
   const handleAdd = async () => {
     setSubmitting(true)
     try {
-      const res = await fetch(`${env.BLOX_FUNCTION_URL}/addTodo`, {
+      const res = await fetch(`${env.BLOX_FUNCTION_URL}/addTodoSaaS`, {
         method: 'post',
         body: JSON.stringify({ name: todo }),
       })
